14b384dbe8cb99a964fe8f5b63803e92bf3ee615
kash321 <kashtilia@gmail.com>
2023-09-17 18:14:28 +0300

    fix api key error

--- app/api/send-email/route.tsx
+++ app/api/send-email/route.tsx
@@ -2,15 +2,17 @@ import {Resend} from "resend";
 import {NextRequest, NextResponse} from "next/server";
 import WelcomeTemplate from "@/emails/WelcomeTemplate";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// Нужно создать экземпляр класса Resend для отправки
+// И использовать API ключ отправки
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: NextRequest) {
-    await resend.emails.send({
-        from: '...',
-        to: 'kashtilia@gmail.com',
-        subject: '...',
-        react: <WelcomeTemplate name="Ilia" />
-    });
+    // await resend.emails.send({
+    //     from: '...',
+    //     to: 'kashtilia@gmail.com',
+    //     subject: '...',
+    //     react: <WelcomeTemplate name="Ilia" />
+    // });
 
     return NextResponse.json({});
 }
